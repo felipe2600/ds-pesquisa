d14cb5ee3717cff5679c66cac748864dad534576
Felipe Silva <felipesilva2600@gmail.com>
2021-12-15 21:31:15 -0300

    Entities

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
@@ -0,0 +1,89 @@
+package com.devsuperior.dspesquisa.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+public class Record implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Integer age;
+	private Instant moment;
+	
+	private Game game;
+	
+	public Record() {
+	}
+
+	public Record(Long id, String name, Integer age, Instant moment, Game game) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.moment = moment;
+		this.game = game;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Record other = (Record) obj;
+		return Objects.equals(id, other.id);
+	}
+}
